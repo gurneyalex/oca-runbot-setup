198411e057fe4120dc6cf26721564a801527852f
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2019-04-26 14:41:04 +0200

    allow override fqdn from config file

--- specific-parts/specific-addons/runbot-oca/__init__.py
+++ specific-parts/specific-addons/runbot-oca/__init__.py
@@ -1,1 +1,10 @@
+from openerp.addons.runbot import runbot as orig_runbot
+from openerp.tools import config
+import socket
+
+def fqdn():
+    return config.get('fqdn', socket.getfqdn())
+
+orig_runbot.fqdn = fqdn
+
 from . import runbot
