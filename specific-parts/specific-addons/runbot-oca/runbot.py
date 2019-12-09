918b37b9e71fd87f9d7c4de0e197cebab1ee596b
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2019-12-09 15:28:44 +0100

    add logging

--- specific-parts/specific-addons/runbot-oca/runbot.py
+++ specific-parts/specific-addons/runbot-oca/runbot.py
@@ -69,8 +69,10 @@ class RunbotBranch(models.Model):
 
     @api.model
     def cron_branch_info(self):
+        _logger.warning('update branch info cron')
         branches = self.search([('branch_name', '=', False)], limit=500, order='id DESC')
         branches._get_branch_infos()
+        _logger.warning('end update branch info cron')
         return True
 
 
