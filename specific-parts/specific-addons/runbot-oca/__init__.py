3cb07878df8c7ebe76d0613d7b09b1e238eb19b3
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2019-12-29 22:37:21 +0100

    fix the import fix

--- specific-parts/specific-addons/runbot-oca/__init__.py
+++ specific-parts/specific-addons/runbot-oca/__init__.py
@@ -1,10 +1,15 @@
-from odoo.addons.runbot import common as orig_runbot
+import odoo.addons.runbot.common as orig_runbot_common
+import odoo.addons.runbot.models.repo as orig_runbot_repo
+import odoo.addons.runbot.models.build as orig_runbot_build
 from odoo.tools import config
 import socket
 
 def fqdn():
     return config.get('fqdn', socket.getfqdn())
 
-orig_runbot.fqdn = fqdn
+orig_runbot_common.fqdn = fqdn
+
+orig_runbot_models_repo.fqdn = fqdn
+orig_runbot_models_build.fqdn = fqdn
 
 from . import runbot
